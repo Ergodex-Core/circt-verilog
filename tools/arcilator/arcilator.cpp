640335f3bd7ba096a4f83f1e3873d9ad3f515fbb
AmurG <gublaghose@gmail.com>
2025-12-24 02:54:47 +0000

    arcilator: accept FIRRTL HW inputs
    - Skip Moore->Core when input has no moore dialect ops
    
    - Fix firtool static link order for Moore/LLHD deps

--- tools/arcilator/arcilator.cpp
+++ tools/arcilator/arcilator.cpp
@@ -244,7 +244,7 @@ static bool untilReached(Until until) {
 
 /// Populate a pass manager with the arc simulator pipeline for the given
 /// command line options. This pipeline lowers modules to the Arc dialect.
-static void populateHwModuleToArcPipeline(PassManager &pm) {
+static void populateHwModuleToArcPipeline(PassManager &pm, bool inputHasMoore) {
   if (verbosePassExecutions)
     pm.addInstrumentation(
         std::make_unique<VerbosePassInstrumentation<mlir::ModuleOp>>(
@@ -255,7 +255,8 @@ static void populateHwModuleToArcPipeline(PassManager &pm) {
   // represented as intrinsic ops.
   if (untilReached(UntilPreprocessing))
     return;
-  pm.addPass(createConvertMooreToCorePass());
+  if (inputHasMoore)
+    pm.addPass(createConvertMooreToCorePass());
   pm.addPass(om::createStripOMPass());
   pm.addPass(emit::createStripEmitPass());
   pm.addPass(createLowerFirMemPass());
@@ -406,7 +407,12 @@ static LogicalResult processBuffer(
   pmArc.enableTiming(ts);
   if (failed(applyPassManagerCLOptions(pmArc)))
     return failure();
-  populateHwModuleToArcPipeline(pmArc);
+  bool inputHasMoore = false;
+  module->walk([&](Operation *op) {
+    if (auto *dialect = op->getDialect())
+      inputHasMoore |= dialect->getNamespace() == "moore";
+  });
+  populateHwModuleToArcPipeline(pmArc, inputHasMoore);
 
   if (failed(pmArc.run(module.get())))
     return failure();
